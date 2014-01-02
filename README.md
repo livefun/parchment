debe117cdd2a76a8b0eaf9eb7d45ab21aa07bb98
livefun <starpig@live.com>
2014-01-02 14:34:37 +0800

    init parchment

--- README.md
+++ README.md
@@ -3,17 +3,24 @@ parchment
 
 #what's this?
 顾名思义 parchment是用来记录的~ 
+
 记录遇到的问题，解决问题的方法 and more
 
 #为什么要做parchment?
  1.需要一个简洁、纯粹的blog来替代大脑 做持久化存储
+
  2.帮助团队新人学习python and 磨合团队
 
 
 #主要技术
  1.语言 -- python
+
  2.web框架 -- flask
+
  3.DB -- mongodb
+
  4.cache -- redis
+
  5.wsgi server -- gunicorn & gevent
+
  6.proxy & static server -- nginx
---
+++ app.py
@@ -0,0 +1,63 @@
+#-*- coding:utf-8 -*-
+from flask import render_template,  request,session ,Flask, current_app
+from flask_principal import Principal, Permission, UserNeed, RoleNeed, Identity, AnonymousIdentity, identity_changed, identity_loaded
+from datetime import datetime
+import simplejson as json
+
+
+
+from logging import Formatter
+import logging.handlers,logging
+
+LOG_FILENAME = 'logs/blog.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=100000000, backupCount=5)
+handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(process)d - Line:%(lineno)d - %(message)s'))
+
+app = Flask(__name__,static_folder='static', template_folder='templates')
+app.logger.setLevel(logging.DEBUG)
+app.logger.addHandler(handler)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
+# load the extension
+principals = Principal(app)
+
+# Create a permission with a single Need, in this case a RoleNeed.
+admin_permission = Permission(RoleNeed('admin'))
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return render_template('errors.html',message ="unauthorized" ), 401
+
+@app.errorhandler(400)
+def page_not_found(e):
+    return render_template('errors.html', message=e.description), 400
+
+@app.errorhandler(403)
+def access_forbidden(e):
+    return render_template('errors.html', message=e.description), 403
+
+@app.errorhandler(404)
+def access_notfound(e):
+    return render_template('errors.html', message='The requested URL was not found on the server. '), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('index.html'), 500
+
+@app.before_request
+def before_request():
+    """Make sure we are connected to the database each request."""
+    #p = request.path.split(".")
+    #plist = ["js","css","png","ico"]
+    #if request.path != '/auth' and request.path != "/login" and p[len(p)-1] not in plist:
+    if 'index' in request.path :
+        return index()
+    app.logger.info("request path:  %s" %(request.path))
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8090)
